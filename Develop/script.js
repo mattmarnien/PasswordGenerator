1aaabc56c0d2d1bbdb3156b6355b881e5b898ab6
mgm215 <60325909+mgm215@users.noreply.github.com>
2020-02-07 14:28:35 -0500

    updated script to clean up conditionals and began adding checkbox functionalit

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passText = document.querySelector("#password");
+var lowerCheck = document.querySelector("#lowerCheck");
+var upperCheck = document.querySelector("#upperCheck");
+var numCheck = document.querySelector("#numCheck");
+var specialCheck = document.querySelector("#specialCheck");
 
 // Arrays from which the password will be pulled
 var lowerArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
@@ -9,7 +14,70 @@ var specialArr = [' ','!','"','#','$','%','&',"'",'(',')','*','+','-','=','_',',
 var passwordArr = [];
 
 // Write password to the #password input
-// function writePassword() {
+function writePassword() {
+var passLength = prompt("Enter the desired length of your password. (Between 8 and 128 characters)");
+while(passLength < 8 || passLength > 128) {
+  alert("Password length must be a number between 8 and 128 characters.");
+  passLength = prompt("Enter the desired length of your password. (Between 8 and 128 characters)");
+}
+
+useLower = lowerCheck.
+
+// var useLower = confirm("Do you want your password to include lower case letters?");
+
+// var useUpper = confirm("Do you want your password to include upper case letters?");
+// var useNum = confirm("Do you want your password to include numbers?");
+// var useSpecial =confirm("Do you want your password to include special characters?");
+// while(useLower == false && useUpper == false && useNum == false && useSpecial == false)
+
+
+// Establishes array password is made from
+
+if(useLower){
+  passwordArr = passwordArr.concat(lowerArr);
+  console.log(passwordArr);
+}
+if(useUpper){
+  passwordArr = passwordArr.concat(upperArr);
+  console.log(passwordArr);
+}
+if(useNum){
+  passwordArr = passwordArr.concat(numArr);
+  console.log(passwordArr);
+}
+if(useSpecial){
+  passwordArr = passwordArr.concat(specialArr);
+  console.log(passwordArr);
+}
+
+if(useLower == false && useUpper == false && useNum == false && useSpecial == false) {
+  alert("Password must include at least one of lower case letters, upper case letters, numbers, or special characters");
+}
+ console.log(passwordArr);
+
+  // Math.floor(Math.random*passwordArr.length)
+
+  var password = "";
+
+  for(var i=0; i < passLength; i++){
+    console.log(password);
+    var x = passwordArr[(Math.floor(Math.random()*passwordArr.length))];
+    console.log(x);
+    password = password + x;
+    console.log(password);
+  }
+  // var password = writePassword(passwordArr, passLength);
+  // console.log(password);
+
+  passText.textContent = password;
+  passwordArr =[];
+}
+
+  
+
+
+
+
 //   var pass = "";
 //   for(var i; i <= passwordArr.length; i++){
 //     console.log(pass);
@@ -24,76 +92,12 @@ var passwordArr = [];
 // }
 
 // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+ generateBtn.addEventListener("click", writePassword);
 
 
 
 
 
 
-var passLength = prompt("Enter the desired length of your password. (Between 8 and 128 characters)");
-var useLower = confirm("Do you want your password to include lower case letters?");
-
-var useUpper = confirm("Do you want your password to include upper case letters?");
-var useNum = confirm("Do you want your password to include numbers?");
-var useSpecial =confirm("Do you want your password to include special characters?");
-
-// Establishes array password is made from
-
-if(useLower && useUpper && useNum && useSpecial){
-  passwordArr = lowerArr.concat(upperArr, numArr, specialArr);
-  console.log(passwordArr.length);
-  } else if(useLower && useNum && useSpecial){
-    passwordArr = lowerArr.concat(numArr, specialArr);
-    console.log(passwordArr.length)
-  } else if(useLower && useNum){
-    passwordArr = lowerArr.concat(numArr);
-    console.log(passwordArr.length)
-  } else if(useLower && useSpecial){
-    passwordArr = lowerArr.concat(specialArr);
-    console.log(passwordArr.length)
-  } else if(useLower){
-    passwordArr = lowerArr;
-    console.log(passwordArr.length)
-  } else if(useUpper && useNum && useSpecial){
-    passwordArr = upperArr.concat(numArr, specialArr);
-    console.log(passwordArr.length)
-  } else if(useUpper && useNum){
-    passwordArr = numArr.concat(numArr);
-    console.log(passwordArr.length)
-  } else if(useUpper && useSpecial){
-    passwordArr = upperArr.concat(specialArr);
-    console.log(passwordArr.length)
-  }  else if(useUpper){
-    passwordArr = upperArr;
-    console.log(passwordArr.length)
-  }  else if(useNum && useSpecial){
-    passwordArr = numArr.concat(specialArr);
-    console.log(passwordArr.length)
-  } else if(useNum){
-    passwordArr = numArr;
-    console.log(passwordArr.length)
-  } else if(useSpecial){
-    passwordArr = specialArr;
-    console.log(passwordArr.length)
-  } else {alert("Password must include at least one of lower case letters, upper case letters, numbers, or special characters");}
-
-  console.log(passwordArr);
-
-  Math.floor(Math.random*passwordArr.length)
-
-  var password = "";
-
-  for(var i=0; i < passLength; i++){
-    console.log(password);
-    var x = passwordArr[(Math.floor(Math.random()*passwordArr.length))];
-    console.log(x);
-    password = password + x;
-    console.log(password);
-  }
-  // var password = writePassword(passwordArr, passLength);
-  // console.log(password);
-
-  alert(password);
 
 
