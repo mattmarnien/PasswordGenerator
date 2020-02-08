5bdde7e3bd5e2fc7d29ca2c64ac7bb79af342525
mattmarnien <mgm215@gmail.com>
2020-02-07 20:52:23 -0500

    doubled size of numArr to increase the likelihood of numbers appearing in pass

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var alertText = document.querySelector("#alertText");
 // Arrays from which the password will be pulled
 var lowerArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var numArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var numArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0','1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var specialArr = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', '-', '=', '_', ',', '.', ':', ';', '@', '[', ']', '/', '^', '`', '|', '{', '}'];
 var passwordArr = [];
 
